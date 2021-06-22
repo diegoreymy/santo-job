35d540d86c43745109a8fb36616ddf1b761fa4cf
Diego H. Reymy A <diegoreymy@gmail.com>
2021-06-22 20:32:15 -0300

    feat(header): comportamento do menu no mobile

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -18,6 +18,7 @@ export class HeaderComponent implements OnInit {
   }
 
   onClickLink(itemMenuSelected: number) {
+    this.open = false;
     this.onItemMenuSelected.emit(itemMenuSelected);
   }
 }
