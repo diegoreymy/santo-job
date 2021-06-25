84cec79ed8abd2c9c2bd34fa903d26906e06fba0
Diego H. Reymy A <diegoreymy@gmail.com>
2021-06-24 23:43:58 -0300

    feat(services): criando card do servico

---
+++ src/app/pages/services/components/cards-carousel/cards-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-cards-carousel]',
+  templateUrl: './cards-carousel.component.html',
+  styleUrls: ['./cards-carousel.component.scss']
+})
+export class CardsCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/services/services.module.ts
+++ src/app/pages/services/services.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ServicesRoutingModule } from './services-routing.module';
 import { ServicesComponent } from './services.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CardsCarouselComponent } from './components/cards-carousel/cards-carousel.component';
 
 
 @NgModule({
   declarations: [
-    ServicesComponent
+    ServicesComponent,
+    CardsCarouselComponent
   ],
   imports: [
     CommonModule,
