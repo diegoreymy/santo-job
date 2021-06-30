181eea0cca63d56c8766de7fc31cb6e0831449a0
Diego H. Reymy A <diegoreymy@gmail.com>
2021-06-29 23:54:37 -0300

    feat(cards-carousel): colocando carrossel de images na tela de servico

--- src/app/pages/services/components/cards-carousel/cards-carousel.component.ts
+++ src/app/pages/services/components/cards-carousel/cards-carousel.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper/core";
+
+SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 @Component({
   selector: '[app-cards-carousel]',
   templateUrl: './cards-carousel.component.html',
-  styleUrls: ['./cards-carousel.component.scss']
+  styleUrls: ['./cards-carousel.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CardsCarouselComponent implements OnInit {
+
+  @Input() imagesList: string[] = []
 
   constructor() { }
 
--- src/app/shared/constants/services-list.constant.ts
+++ src/app/shared/constants/services-list.constant.ts
@@ -2,67 +2,140 @@ export interface IServiceList {
     name: string;
     link: string;
     img: string;
+    imagesList: string[];
 }
 
 export const SERVICES_LIST: IServiceList[] = [
     {
         name: 'Publicidade',
         link: '/servicos/publicidade',
-        img: '/assets/images/icons/icon-publicidade.png'
+        img: '/assets/images/icons/icon-publicidade.png',
+        imagesList: [
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+        ]
     },
     {
         name: 'Incentivo',
         link: '/servicos/incentivo',
-        img: '/assets/images/icons/icon-incentivo.png'
+        img: '/assets/images/icons/icon-incentivo.png',
+        imagesList: [
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+        ]
     },
     {
         name: 'Planejamento estratégico',
         link: '/servicos/planejamento-estrategico',
-        img: '/assets/images/icons/icon-planejamento.png'
+        img: '/assets/images/icons/icon-planejamento.png',
+        imagesList: [
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+        ]
     },
     {
         name: 'Trade Marketing',
         link: '/servicos/trade-marketing',
-        img: '/assets/images/icons/icon-trade.png'
+        img: '/assets/images/icons/icon-trade.png',
+        imagesList: [
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+        ]
     },
     {
         name: 'Influencer',
         link: '/servicos/influencer',
-        img: '/assets/images/icons/icon-influencer.png'
+        img: '/assets/images/icons/icon-influencer.png',
+        imagesList: [
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+        ]
     },
     {
         name: 'Logos e selos',
         link: '/servicos/logos-e-selos',
-        img: '/assets/images/icons/icon-logos-selos.png'
+        img: '/assets/images/icons/icon-logos-selos.png',
+        imagesList: [
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+        ]
     },
     {
         name: 'Promoção',
         link: '/servicos/promocao',
-        img: '/assets/images/icons/icon-promotion.png'
+        img: '/assets/images/icons/icon-promotion.png',
+        imagesList: [
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+        ]
     },
     {
         name: 'Publi Digital',
         link: '/servicos/publi-digital',
-        img: '/assets/images/icons/icon-publi-digital.png'
+        img: '/assets/images/icons/icon-publi-digital.png',
+        imagesList: [
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+        ]
     },
     {
         name: 'Projetos proprietários',
         link: '/servicos/projetos-proprietarios',
-        img: '/assets/images/icons/icon-project.png'
+        img: '/assets/images/icons/icon-project.png',
+        imagesList: [
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+        ]
     },
     {
         name: 'Eventos',
         link: '/servicos/eventos',
-        img: '/assets/images/icons/icon-event.png'
+        img: '/assets/images/icons/icon-event.png',
+        imagesList: [
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+        ]
     },
     {
         name: 'Endomarketing',
         link: '/servicos/endomarketing',
-        img: '/assets/images/icons/icon-endomarketing.png'
+        img: '/assets/images/icons/icon-endomarketing.png',
+        imagesList: [
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+        ]
     },
     {
         name: 'Assessoria de imprensa',
         link: '/servicos/assessoria-de-imprensa',
-        img: '/assets/images/icons/icon-press.png'
+        img: '/assets/images/icons/icon-press.png',
+        imagesList: [
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+          '/assets/images/tirulipa.png',
+        ]
     }
 ]
